df262a8dab044843b25da20979c9be92457f3e3c
pedrocabralia <pedrocabralia@hotmail.com>
2022-01-02 12:36:46 -0300

    add image

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var app = new Vue({   //creating a vue instance
     el: '#app',   // this is going to connect to the div up here
     data:{
 
-        Product:'Socks'
+        Product:'Socks',
+        image: './assets/vmSocks-green-onWhite.jpg'
     }
 
 });
